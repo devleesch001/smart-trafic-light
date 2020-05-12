094750b55478d0e2a1b7a3f2f4f574ff071b6cb2
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2020-05-12 14:12:05 +0200

    add inter_com file

---
+++ Core/Inc/inter_com.h
@@ -0,0 +1,13 @@
+/*
+ * inter_comuncation.h
+ *
+ *  Created on: May 12, 2020
+ *      Author: dev-
+ */
+
+#ifndef INC_INTER_COM_H_
+#define INC_INTER_COM_H_
+
+
+
+#endif /* INC_INTER_COM_H_ */
---
+++ Core/Src/inter_com.c
@@ -0,0 +1,8 @@
+/*
+ * inter_com.c
+ *
+ *  Created on: May 12, 2020
+ *      Author: dev-
+ */
+
+
