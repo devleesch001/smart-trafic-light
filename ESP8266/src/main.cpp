91e53e54c55ffb8a3354e92d17740d139317204e
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2020-05-12 19:53:52 +0200

    add function getDataFromSensor

--- ESP8266/src/main.cpp
+++ ESP8266/src/main.cpp
@@ -2,16 +2,47 @@
 #include <ArduinoJson.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
 
 const char *ssid = "Livebox-dev-";
 const char *password = "79221FC83787F575CE711C1668";
 
+SoftwareSerial Uart2(0, 2);
+
 int a = 0;
+
+//**************** Prototypes ****************
+bool connectToWIFI(int tryConnect);
+String getData(const char * data);
+
+
+//****************setup*****************
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(9600); // start serial port at 9600 bps
+    Serial.println("---------- Program Start ----------");
+
+    Uart2.begin(9600);
+    delay(1000);
+
+    while (!connectToWIFI(20)) {}       // connect to WIFI try 20
+}
+
+//****************loop*****************
+void loop() {
+    // put your main code here, to run repeatedly:
+
+    if (Serial.available() > 0) {
+        // read the incoming byte:
+        Serial.println(getDataFromSensor());
+    }
+
+    
+}
 
 //****************Functions*****************
 
 //***Function for connect to internet 
-// try during tpssec seconds
 bool connectToWIFI(int tryConnect) {
     
     Serial.print("Waiting for connection to WiFi to : ");
@@ -33,31 +64,13 @@ bool connectToWIFI(int tryConnect) {
     return true;
 }
 
-//***Function for recover data.. but as we can't plug elements on our board, this will be fake data:
-// temp is random, mac is write in the function, always the same, et serial number is in the parameter
-//The function update the global array variable data
-
-
-//***Function wich send data to the server
-
-//****************setup*****************
-void setup() {
-    // put your setup code here, to run once:
-    Serial.begin(9600);                   // start serial port at 9600 bps
-    delay(1000);
-    Serial.println("---------- Program Start ----------");
-    while (!connectToWIFI(20)) {}       // connect to WIFI
-
+String getDataFromSensor() {
+    String dataFromSensor = "";
     
-}
-
-//****************loop*****************
-void loop() {
-    // put your main code here, to run repeatedly:
-
-    if (Serial.available() > 0) {
-        // read the incoming byte:
-        String data = Serial.readString();
-        Serial.println(data);
+    while(Uart2.available() > 0) {
+        dataFromSensor = Uart2.readString(); //read() read from RX_PIN  character by character 
     }
+    Serial.println(dataFromSensor);
+
+    return dataFromSensor;
 }
