beab072254bf8669feb99b1c2ef35c4a01dfa298
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2020-05-19 10:52:57 +0200

    add ToF function it use API vl53l0x

--- STM32/Core/Inc/ToF.h
+++ STM32/Core/Inc/ToF.h
@@ -13,6 +13,8 @@
 // HAL
 #include "stm32g0xx_hal.h"
 #include "stm32g0xx_ll_system.h"
+#include "main.h"
+#include "cmsis_os.h"
 
 // OS
 #include "cmsis_os2.h"
@@ -22,10 +24,23 @@
 #include <stdbool.h>	// bool
 #include <string.h>		// strlen...
 
+
+// API
+#include "vl53l0x_api.h"
+
+// Debug
+#include "osdebug.h"
+
+#define I2CHANDLE		hi2c1
+
 /* functions prototypes ---------------------------------------------*/
 
 void VL53L_init(void);
-void ToF_Read(void);
-
+void ToF(void);
+bool ToF_init(VL53L0X_DEV device, bool debug);
+VL53L0X_Error getSingleRanging (VL53L0X_DEV device,VL53L0X_RangingMeasurementData_t *RangingMeasurementData, bool debug);
+int32_t VL53L0X_read_multi(uint8_t address, uint8_t index, uint8_t *pdata, uint32_t count);
+void print_range_status(VL53L0X_RangingMeasurementData_t *pRangingMeasurementData);
+void print_pal_error(VL53L0X_Error Status);
 
 #endif /* INC_TOF_H_ */
--- STM32/Core/Src/ToF.c
+++ STM32/Core/Src/ToF.c
@@ -7,69 +7,193 @@
 
 #include "ToF.h"
 
-/*
-#define VL53L_ADDR 0xD0
 
-#define SMPLRT_DIV_REG 0x19
-#define GYRO_CONFIG_REG 0x1B
-#define ACCEL_CONFIG_REG 0x1C
-#define ACCEL_XOUT_H_REG 0x3B
-#define TEMP_OUT_H_REG 0x41
-#define GYRO_XOUT_H_REG 0x43
-#define PWR_MGMT_1_REG 0x6B
-#define WHO_AM_I_REG 0x75
+//#define VL53L_ADDR 0xD0 << 1
+
+/* Private variables ---------------------------------------------------------*/
+
+extern I2C_HandleTypeDef I2CHANDLE;
+
+bool ToF_init(VL53L0X_DEV device, bool debug){
+
+	uint32_t refSpadCount;
+	uint8_t isApertureSpads;
+	uint8_t VhvSettings;
+	uint8_t PhaseCal;
+
+	VL53L0X_Error Status;
+
+	device->I2cDevAddr      = 0x29 << 1;
+	device->comms_type      = 1;
+	device->comms_speed_khz = 400;
 
 
+	VL53L0X_DeviceInfo_t Info;
+	VL53L0X_DeviceInfo_t *pInfo = &Info;
 
-void VL53L_init(void)
-{
-	uint8_t check;
-	uint8_t Data;
+	Status = VL53L0X_DataInit(device);
 
-	// check device ID WHO_AM_I
+	if (Status == VL53L0X_ERROR_NONE) {
 
-	HAL_I2C_Mem_Read (&hi2c1, VL53L_ADDR, WHO_AM_I_REG, 1, &check, 1, 1000);
+	Status = VL53L0X_GetDeviceInfo(device, pInfo);
+		if (debug){
+			dbg_printfln("VL53L0X Info:");
+			dbg_printfln("Device Name: %s", Info.Name);
+			dbg_printfln("Type: %s", Info.Type);
+			dbg_printfln("ID: %s", Info.ProductId);
+			dbg_printfln("Rev Major: %u Minor: %u, %u\r\n", Info.ProductRevisionMajor, Info.ProductRevisionMinor, Info.ProductType);
+		}
 
-	printf("I2C_check = %u");
-	if (check == 114)  // 0x68 will be returned by the sensor if everything goes well
-	{
-
-		// power management register 0X6B we should write all 0's to wake the sensor up
-		Data = 0;
-		HAL_I2C_Mem_Write(&hi2c1, VL53L_ADDR, PWR_MGMT_1_REG, 1,&Data, 1, 1000);
-
-		// Set DATA RATE of 1KHz by writing SMPLRT_DIV register
-		Data = 0x07;
-		HAL_I2C_Mem_Write(&hi2c1, VL53L_ADDR, SMPLRT_DIV_REG, 1, &Data, 1, 1000);
-
-		// Set accelerometer configuration in ACCEL_CONFIG Register
-		// XA_ST=0,YA_ST=0,ZA_ST=0, FS_SEL=0 -> � 2g
-		Data = 0x00;
-		HAL_I2C_Mem_Write(&hi2c1, VL53L_ADDR, ACCEL_CONFIG_REG, 1, &Data, 1, 1000);
-
-		// Set Gyroscopic configuration in GYRO_CONFIG Register
-		// XG_ST=0,YG_ST=0,ZG_ST=0, FS_SEL=0 -> � 250 �/s
-		Data = 0x00;
-		HAL_I2C_Mem_Write(&hi2c1, VL53L_ADDR, GYRO_CONFIG_REG, 1, &Data, 1, 1000);
 	}
 
+	if (Status == VL53L0X_ERROR_NONE) {
+		if (debug) {
+			dbg_printfln("VL53L0X: StaticInit");
+		}
+
+		Status = VL53L0X_StaticInit(device);
+
+	}
+
+	if (Status == VL53L0X_ERROR_NONE) {
+	        if (debug) {
+	        	dbg_printfln("VL53L0X: PerformRefSpadManagement");
+	        }
+
+	        Status = VL53L0X_PerformRefSpadManagement(device, &refSpadCount, &isApertureSpads); // Device Initialization
+
+	        if (debug) {
+	        	dbg_printfln("refSpadCount = %lu, isApertureSpads %u", refSpadCount, isApertureSpads);
+	        }
+	    }
+
+	if (Status == VL53L0X_ERROR_NONE) {
+		if (debug) {
+			dbg_printfln("VL53L0X: PerformRefCalibration");
+		}
+
+		Status = VL53L0X_PerformRefCalibration(device, &VhvSettings, &PhaseCal); // Device Initialization
+	}
+
+    if (Status == VL53L0X_ERROR_NONE) {
+        // no need to do this when we use VL53L0X_PerformSingleRangingMeasurement
+        if (debug) {
+        	dbg_printfln("VL53L0X: SetDeviceMode");
+        }
+
+        Status = VL53L0X_SetDeviceMode(device, VL53L0X_DEVICEMODE_SINGLE_RANGING); // Setup in single ranging mode
+    }
+
+    if (Status == VL53L0X_ERROR_NONE) {
+		Status = VL53L0X_SetLimitCheckEnable(device, VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE, 1);
+    }
+
+    if (Status == VL53L0X_ERROR_NONE) {
+        Status = VL53L0X_SetLimitCheckEnable(device, VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, 1);
+    }
+
+    if (Status == VL53L0X_ERROR_NONE) {
+        Status = VL53L0X_SetLimitCheckEnable(device, VL53L0X_CHECKENABLE_RANGE_IGNORE_THRESHOLD, 1);
+    }
+
+    if (Status == VL53L0X_ERROR_NONE) {
+    	Status = VL53L0X_SetLimitCheckValue(device, VL53L0X_CHECKENABLE_RANGE_IGNORE_THRESHOLD,(FixPoint1616_t) (1.5 * 0.023 * 65536));
+    }
+
+	if (debug){
+		print_pal_error(Status);
+	}
+
+
+    if (Status == VL53L0X_ERROR_NONE) {
+		return true;
+    } else {
+
+    	return false;
+    }
 }
 
-void ToF_Read(void)
-{
-	uint8_t Rec_Data[6];
+VL53L0X_Error getSingleRanging (VL53L0X_DEV device, VL53L0X_RangingMeasurementData_t *RangingMeasurementData, bool debug) {
+	//dbg_printfln("\r\ngetSingleRanging() has started\r\n");
 
-	// Read 6 BYTES of data starting from ACCEL_XOUT_H register
+	VL53L0X_Error Status;
+	FixPoint1616_t LimitCheckCurrent;
 
-	HAL_I2C_Mem_Read (&hi2c1, VL53L_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 6, 1000);
+	Status = VL53L0X_PerformSingleRangingMeasurement(device, RangingMeasurementData);
 
-	Accel_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data [1]);
-	Accel_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data [3]);
-	Accel_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data [5]);
+	if (Status == VL53L0X_ERROR_NONE) {
 
-	Ax = Accel_X_RAW/16384.0;
-	Ay = Accel_Y_RAW/16384.0;
-	Az = Accel_Z_RAW/16384.0;
+		if (debug) {
+
+		}
+
+		if (debug) {
+			Status = VL53L0X_GetLimitCheckCurrent(device, VL53L0X_CHECKENABLE_RANGE_IGNORE_THRESHOLD, &LimitCheckCurrent);
+
+			if (Status == VL53L0X_ERROR_NONE) {
+				dbg_printfln("RANGE IGNORE THRESHOLD: %f", LimitCheckCurrent / 65536.0);
+				print_pal_error(Status);
+			}
+
+			dbg_printfln("Measured distance : %d", RangingMeasurementData->RangeMilliMeter);
+			print_pal_error(Status);
+		}
+
+	}
+
+
+	return Status;
 }
 
-*/
+void print_range_status(VL53L0X_RangingMeasurementData_t* pRangingMeasurementData){
+    char buf[VL53L0X_MAX_STRING_LENGTH];
+    uint8_t RangeStatus;
+
+    /*
+     * New Range Status: data is valid when pRangingMeasurementData->RangeStatus = 0
+     */
+
+    RangeStatus = pRangingMeasurementData->RangeStatus;
+
+    VL53L0X_GetRangeStatusString(RangeStatus, buf);
+    dbg_printfln("Range Status: %i : %s", RangeStatus, buf);
+
+}
+
+void print_pal_error(VL53L0X_Error Status){
+    char buf[VL53L0X_MAX_STRING_LENGTH];
+    VL53L0X_GetPalErrorString(Status, buf);
+    dbg_printfln("API Status: %i : %s", Status, buf);
+}
+
+void ToF(void){
+
+	//VL53L0X_PerformSingleRangingMeasurement
+}
+
+/*
+	VL53L0X_Error Status = VL53L0X_ERROR_NONE;
+	VL53L0X_Dev_t Device;
+	VL53L0X_Dev_t *device = &Device;
+
+	device->I2cDevAddr      = 0x29 << 1;
+	device->comms_type      =  1;
+	device->comms_speed_khz =  400;
+
+	uint8_t byteData;
+	uint16_t wordData;
+
+	uint8_t ProductRevisionMajor, ProductRevisionMinor;
+
+	uint8_t *pProductRevisionMajor = &ProductRevisionMajor;
+	uint8_t *pProductRevisionMinor = &ProductRevisionMinor;
+
+	dbg_printfln("Status : %u", Status);
+	Status = VL53L0X_GetProductRevision(device, pProductRevisionMajor, pProductRevisionMinor);
+
+
+	dbg_printfln("Status : %u", Status);
+
+	dbg_printfln("Major : %u", ProductRevisionMajor);
+	dbg_printfln("Minor : %u", ProductRevisionMinor);
+ */
